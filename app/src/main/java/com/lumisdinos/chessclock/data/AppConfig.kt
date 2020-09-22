863597cbe070fc50706220c9a51759d412c7100e
LumisD <lumisdinos@gmail.com>
2020-09-22 11:53:21 -0300

    Minors

--- app/src/main/java/com/lumisdinos/chessclock/data/AppConfig.kt
+++ app/src/main/java/com/lumisdinos/chessclock/data/AppConfig.kt
@@ -2,9 +2,9 @@ package com.lumisdinos.chessclock.data
 
 object AppConfig {
 
-    var min = 0//15
-    var sec = 3//0
-    var inc = 1//10
+    var min = 15
+    var sec = 0
+    var inc = 10
 
 
     var previousClickTimeMillis = 0L
